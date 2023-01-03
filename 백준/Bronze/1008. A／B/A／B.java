e6e8bdc17c5f8068b84810e2a5d669f37cb4dfcf
leeshinbi <tlsql6831@naver.com>
2023-01-03 23:41:03 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Main {
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	
+	double A,B,sum;
+	
+	A = sc.nextDouble();
+	B = sc.nextDouble();
+	
+	sum = A/B;
+	System.out.println(sum);
+	
+}
+}
