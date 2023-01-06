79c9a814ee3a346daf9e1badb4eeccadf0947269
leeshinbi <tlsql6831@naver.com>
2023-01-06 17:06:40 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class Main {
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	
+ int y; 
+ y = sc.nextInt();
+ 
+ int sum = y-543;
+
+ System.out.println(sum);
+ 
+}
+}
