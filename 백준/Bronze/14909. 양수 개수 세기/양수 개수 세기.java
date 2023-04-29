0afa12da45a3b6efb8cc856dcaa9fce49ec1c122
01_49g_63m <tlsql6831@naver.com>
2023-04-30 00:08:03 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 1700 ms, Memory: 310708 KB -BaekjoonHub

--- 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.java
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int cnt = 0;
-        while (sc.hasNextInt()) {
-            int a = sc.nextInt();
-            if (a > 0) {
+        Scanner in = new Scanner(System.in);
+        int x;
+        int cnt=0;
+        while(in.hasNextInt()){
+            x=in.nextInt();
+            if(x>0)
                 cnt++;
-            }
         }
         System.out.println(cnt);
     }
