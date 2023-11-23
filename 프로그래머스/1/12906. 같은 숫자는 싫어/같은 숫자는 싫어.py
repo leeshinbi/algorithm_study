481e84c16892fd26d2eafd56f8fa81b0a3d86d98
01_49g_63m <tlsql6831@naver.com>
2023-11-24 00:37:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 116.08 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    
+    answer = [arr[0]]
+    
+    for i in range(1,len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
+    
+    return answer 
+  
