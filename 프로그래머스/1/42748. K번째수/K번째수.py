753c60dac2351f0ee02e638e6438128d69083724
신비 <tlsql6831@naver.com>
2023-11-27 01:23:59 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        i,j,k = command
+        new_array = array[i-1:j]
+        new_array.sort()
+        answer.append(new_array[k-1])
+    return answer
