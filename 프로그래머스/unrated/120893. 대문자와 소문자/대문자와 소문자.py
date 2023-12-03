ceb11ab20d5a02be7e4180d8c75049028d62bc49
신비 <tlsql6831@naver.com>
2023-12-03 14:52:34 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    
+    for char in my_string:
+        if char.islower():
+            answer += char.upper()
+        elif char.isupper():
+            answer += char.lower()
+    return answer
